16b1f008987e248df64deeb2dfec0d60278d043d
jcraig0 <jovon.craig@maine.edu>
2018-11-02 19:22:22 +0000

    Generated code by SwaggerHub

--- flask/swagger_server/models/__init__.py
+++ flask/swagger_server/models/__init__.py
@@ -4,6 +4,4 @@
 from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.course import Course
-from swagger_server.models.inventory_item import InventoryItem
-from swagger_server.models.manufacturer import Manufacturer
 from swagger_server.models.template import Template
