7296a1230154098c795224e906355311e2c5cbb0
jcraig0 <jovon.craig@maine.edu>
2018-11-02 19:12:57 +0000

    Generated code by SwaggerHub

--- flask/swagger_server/models/__init__.py
+++ flask/swagger_server/models/__init__.py
@@ -3,5 +3,6 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
+from swagger_server.models.course import Course
 from swagger_server.models.inventory_item import InventoryItem
 from swagger_server.models.manufacturer import Manufacturer
