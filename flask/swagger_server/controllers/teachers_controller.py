8284c2be2ff5d7befa84b0e616faa664fe8bf3fd
jcraig0 <jovon.craig@maine.edu>
2018-11-02 19:32:41 +0000

    Generated code by SwaggerHub

--- flask/swagger_server/controllers/teachers_controller.py
+++ flask/swagger_server/controllers/teachers_controller.py
@@ -8,7 +8,7 @@ from swagger_server import util
 def search_inventory(teacher_id):  # noqa: E501
     """retreives a list of all courses
 
-    By passing in the appropriate options, you can search for available inventory in the system  # noqa: E501
+    Exactly what the summary says  # noqa: E501
 
     :param teacher_id: the ID for a teacher
     :type teacher_id: int
