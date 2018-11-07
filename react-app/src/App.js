ebf9a98f31a5de5e1a99853b25ae9ca873a1d721
Stan Small <stanley.small1@maine.edu>
2018-11-07 13:43:01 -0500

    added new pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
             <button type="button">Start a Survey</button> <br/>
             <button type="button">Log In</button> <br/>
         <br/>
-        Next Page
+        <b> Next Page </b>
         <br/>
         <br/>
         {/*Questions to select course attributes*/}
@@ -22,19 +22,31 @@ class App extends Component {
     Course Section (e.g. 001)
     <input type="text" name="coursesection" /> <br />
     Closing Date
-    <input type="date" /> <br />
+    <input type="date" /> <br/>
     Time of Day
     <input type="time" /> <br />
   <input type="submit" value="Submit" />
 </form>
-
-  // Instructor attributes
+<br/>
+<b> Next Page </b>
+<br/>
+<br/>
 <form>
     How prepared was the instructor for class?
     <input type="checkbox" value="include" /> Include
     <input type="checkbox" value="mandatory" /> Mandatory <br />
+    <form>
+      Add new question
+      <input type="text" name="question" />
+      <input type="submit" value="Add" />
+    </form>
         <input type="submit" value="Submit" />
 </form>
+
+<br/>
+<b> Next Page </b>
+<br/>
+<br/>
         </div>
     );
   }
