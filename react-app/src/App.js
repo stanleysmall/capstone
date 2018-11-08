bf8d5bce3c9c874ebf4d1eceb8b57f7142ac4e31
Sam Elliott <selliott452@gmail.com>
2018-11-07 21:12:55 -0500

    Added some CSS formatting
    Added some CSS formatting and some structure to the components.  I will continue to input the form questions in tomorrow.
    
    Need to work on figuring out how to create new questions.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
-        <div>
+		<form>
+        <div class = "container">
             <Welcome/>
-            <CourseForm/>
-            <InstructorForm/>
+		</div>
+		<br/>
+		
+		<div class = "container">
+        <CourseForm/>
         </div>
+		<br/>
+
+		
+		<div class = "container">
+		<InstructorForm/>
+		</div>
+		
+		</form>
+
     );
   }
 }
@@ -18,8 +32,9 @@ class Welcome extends Component {
     render() {
     return (
         <div>
-            Welcome to EVAL! <br/>
+            <h2>Welcome to EVAL! </h2>
             <button type="button">Start a Survey</button> <br/>
+			<br/>
             <button type="button">Log In</button> <br/>
         </div>
     );
@@ -29,17 +44,80 @@ class Welcome extends Component {
 class CourseForm extends Component {
     render() {
     return (
-        <form>
-    Course Designator (e.g. MUS)
-    <input type="text" name="coursedesignator" /> <br />
-    Course Number (e.g. 200)
-    <input type="text" name="coursenumber" /> <br />
-    Course Section (e.g. 001)
-    <input type="text" name="coursesection" /> <br />
+	
+    <form>
+	
+	<div class="fieldLabel">
+		Course Designator (e.g. MUS)
+	</div>
+	<div class="fieldEntry">
+		<input type="text" name="coursedesignator" placeholder = "" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+		Course Number (e.g. 200)
+	</div>
+	<div class="fieldEntry">
+		<input type="text" name="coursenumber" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+		Course Section (e.g. 001)
+	</div>
+	<div class="fieldEntry">
+		<input type="text" name="coursesection" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+		Course Title (e.g. Ballroom Dance)
+	</div>
+	<div class="fieldEntry">
+		<input type = "text" name = "coursetitle" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+		Semester and Calendar Year (e.g Fall 2019)
+	</div>
+	<div class="fieldEntry">
+		<input type = "text" name = "year" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+	Name of Faculty Unit ( e.g. School of Music)
+	</div>
+	<div class="fieldEntry">
+	<input type = "text" name = "facultyUnit" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+	Name of College ( e.g. Liberal Arts)
+	</div>
+	<div class="fieldEntry">
+	<input type = "text" name = "facultyUnit" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
+	Name of University ( e.g. Liberal Arts)
+	</div>
+	<div class="fieldEntry">
+	<input type = "text" name = "facultyUnit" /> <br />
+	</div>
+	
+	<div class="fieldLabel">
     Closing Date
+	</div>
+	<div class="fieldEntry">
     <input type="date" /> <br/>
+	</div>
+	
+	<div class="fieldLabel">
     Time of Day
+	</div>
+	<div class="fieldEntry">
     <input type="time" /> <br />
+	</div>
+
+	
   <input type="submit" value="Submit" />
 </form>
     );
@@ -49,19 +127,53 @@ class CourseForm extends Component {
 class InstructorForm extends Component {
     render() {
     return (
-        <form>
-    How prepared was the instructor for class?
-    <input type="checkbox" value="include" /> Include
-    <input type="checkbox" value="mandatory" /> Mandatory <br />
     <form>
-      Add new question
-      <input type="text" name="question" />
-      <input type="submit" value="Add" />
-    </form>
-        <input type="submit" value="Submit" />
-</form>
+		<form>
+			<div class="fieldLabel">
+			<input type="text" name="question" value = "How prepared was the instructor for class?"/><br />
+			</div>
+			<input type="checkbox" value="include" /> Include
+			<input type="checkbox" value="mandatory" /> Mandatory <br />
+			 <br /><br />
+			<questionCheckBoxes/>
+		</form>
+		
+		<form>
+			<div class="fieldLabel">
+			<input type="text" name="question"/><br />
+			</div>
+
+			<input type="checkbox" value="include" /> Include
+			<input type="checkbox" value="mandatory" /> Mandatory <br />
+			 <br /><br />
+			<questionCheckBoxes/>
+		</form>
+		
+		<button type="button">Add question</button> <br/><br/>
+		
+		<form>
+		<input type="submit" value="Submit" />
+		</form>
+	</form>
+
     );
   }
+}
+
+class questionCheckBoxes extends Component {
+	render() {
+		return (
+			<form>
+				<div class="fieldLabel">
+				Add new question
+				</div>
+				<div class="fieldEntry">
+				<input type="text" name="question" />
+				</div>
+				<input type="submit" value="Add" />
+			</form>
+		);
+	}
 }
 
 export default App;
