fcdce34797ab3c02e8b1e89ea6c66690a393df0f
Stanley Small <stansmall@users.noreply.github.com>
2019-02-07 10:52:37 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,7 +5,6 @@
 sudo yum update -y
 sudo yum install git -y
 git clone https://github.com/stansmall/capstone.git 
-cd capstone
 sudo yum install -y docker 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
