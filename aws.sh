3b93fbbd7fbb46622d2ee080ee4ab6cfa39016aa
Stanley Small <stansmall@users.noreply.github.com>
2019-02-07 10:56:20 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,14 +5,16 @@
 sudo yum update -y
 sudo yum install git -y
 git clone https://github.com/stansmall/capstone.git 
+cd capstone
+pwd
 sudo yum install -y docker 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 sudo service docker start 
 sudo usermod -a -G docker ec2-user 
+cd react-app
+pwd
 sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash 
 . ~/.nvm/nvm.sh 
 nvm install 11.9.0 
-cd capstone
-cd react-app
 npm install
