f6b6421e32a37d7b28095ac171368b76cefe0632
Stanley Small <stansmall@users.noreply.github.com>
2019-02-07 12:01:45 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -8,6 +8,7 @@ git clone https://github.com/stansmall/capstone.git
 cd capstone
 pwd
 sudo git clone https://github.com/LimeSurvey/LimeSurvey.git
+sudo chmod -R 777 LimeSurvey/
 sudo yum install -y docker 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
