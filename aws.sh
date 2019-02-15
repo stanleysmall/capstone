91d9ea152378c171f56328a6eb7cf517f457279c
Stanley Small <stansmall@users.noreply.github.com>
2019-02-15 10:47:25 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,27 +5,31 @@
 sudo yum update -y
 sudo yum install git -y
 sudo yum install mysql -y
+sudo yum install docker -y 
+
 git clone https://github.com/stansmall/capstone.git 
 cd capstone
 mkdir data
 sudo chmod -R 0777 ./data
 sudo git clone https://github.com/LimeSurvey/LimeSurvey.git
 sudo chmod -R 777 LimeSurvey/
-sudo yum install -y docker 
-sudo service docker start 
-sudo usermod -a -G docker ec2-user 
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
-sudo chmod +x /usr/local/bin/docker-compose 
+
 cd react-app
 sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash 
 . ~/.nvm/nvm.sh 
 nvm install 11.9.0 
 npm install
 npm audit fix
+
 cd ../flask/swagger_server
 git clone https://github.com/TaiSHiNet/lime-py-api.git
 mv lime-py-api lime_py_api
 cd ../..
+
+sudo service docker start 
+sudo usermod -a -G docker ec2-user 
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
+sudo chmod +x /usr/local/bin/docker-compose 
 newgrp docker
 newgrp ec2-user
 docker-compose up -d
