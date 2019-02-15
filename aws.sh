b8bcc4e65756413d0631de0bbec935a80b0fd15a
Stanley Small <stansmall@users.noreply.github.com>
2019-02-14 21:12:05 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -13,9 +13,6 @@ sudo chmod -R 777 LimeSurvey/
 sudo yum install -y docker 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
-sudo service docker start 
-newgrp docker
-newgrp ec2-user
 cd react-app
 pwd
 sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash 
@@ -28,6 +25,9 @@ git clone https://github.com/TaiSHiNet/lime-py-api.git
 mv lime-py-api lime_py_api
 cd ..
 cd ..
+sudo service docker start 
+newgrp docker
+newgrp ec2-user
 docker-compose up -d
 cd data
 sudo chmod -R 0777 ./ 
