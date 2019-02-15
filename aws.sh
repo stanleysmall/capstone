2660112e7db8a7283206e9a20975fca54a24f122
Stanley Small <stansmall@users.noreply.github.com>
2019-02-15 10:30:09 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -12,6 +12,8 @@ sudo chmod -R 0777 ./data
 sudo git clone https://github.com/LimeSurvey/LimeSurvey.git
 sudo chmod -R 777 LimeSurvey/
 sudo yum install -y docker 
+sudo service docker start 
+sudo usermod -a -G docker ec2-user 
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 cd react-app
