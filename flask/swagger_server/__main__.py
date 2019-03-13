e42566874c16b8f2266c371a89aed86a92a9c584
Stanley Small <stansmall@users.noreply.github.com>
2019-03-13 16:24:47 -0400

    Update __main__.py

--- flask/swagger_server/__main__.py
+++ flask/swagger_server/__main__.py
@@ -14,7 +14,7 @@ def main():
     app.app.config['SESSION_TYPE'] = 'redis'
     app.app.config['SESSION_REDIS'] = redis.from_url('redis://10.5.0.2')
     sess = Session()
-    sess.init_app(app)
+    sess.init_app(app.app)
     app.app.config['SECRET_KEY'] = os.urandom(24)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Project Eval API'})
