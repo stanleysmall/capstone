e097155ad20550bd2bc5e88196436d6774cb0903
Stanley Small <stansmall@users.noreply.github.com>
2019-03-13 15:30:05 -0400

    Update __main__.py

--- flask/swagger_server/__main__.py
+++ flask/swagger_server/__main__.py
@@ -8,6 +8,7 @@ from swagger_server import encoder
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.secret_key = os.urandom(24)
+    sess.init_app(app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Project Eval API'})
     app.run(port=8080)
