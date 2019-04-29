9ab9a2f814a4992dd5fe4a0e7e1645f88c929d8c
Stanley Small <10047427+stanleysmall@users.noreply.github.com>
2019-04-29 18:26:21 -0400

    Update __main__.py

--- flask/swagger_server/__main__.py
+++ flask/swagger_server/__main__.py
@@ -15,6 +15,11 @@ def main():
     app.add_api('swagger.yaml', arguments={'title': 'Project Eval API'})
     app.run(port=8080)
     CORS(app)
+    var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    app.use(allowCrossDomain); // plumbing it in as middleware
     logging.getLogger('flask_cors').level = logging.DEBUG
 
 if __name__ == '__main__':
